06ed315707bfe25bb7f51dceb0d6070b13d80a8c
David Day <dj6082013@gmail.com>
2020-05-27 01:30:07 +0800

    remove record and setting page

--- src/main/java/com/david/midimug/PrimaryController.java
+++ src/main/java/com/david/midimug/PrimaryController.java
@@ -16,24 +16,72 @@
  */
 package com.david.midimug;
 
-import java.io.IOException;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 
-public class PrimaryController {
+/**
+ * FXML Controller class
+ *
+ * @author david
+ */
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToPlay() throws IOException {
-        App.setRoot("play");
-    }
-
+    private StackPane keyboard;
     @FXML
-    private void switchToRecord() throws IOException {
-        App.setRoot("record");
+    private StackPane timeline;
+
+    private Button[] whiteKey, blackKey;
+
+    private final int whiteCt = 52, blackCt = 36;
+
+    private File midi;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        renderKeyboard();
     }
 
-    @FXML
-    private void switchToSetting() throws IOException {
-        App.setRoot("setting");
-    }
+    private void renderKeyboard() {
+        keyboard.setAlignment(Pos.TOP_LEFT);
+        ObservableList nodeList = keyboard.getChildren();
 
+        whiteKey = new Button[whiteCt];
+        blackKey = new Button[blackCt];
+
+        final double whiteKeyHeight = keyboard.getPrefHeight();
+        final double whiteKeyWidth = Screen.getPrimary().getBounds().getWidth() / whiteCt;
+        for (int i = 0; i < whiteCt; i++) {
+            Button key = whiteKey[i] = new Button();
+            key.setPrefWidth(whiteKeyWidth);
+            key.setPrefHeight(whiteKeyHeight);
+            key.setTranslateX(i * whiteKeyWidth);
+            nodeList.add(key);
+        }
+
+        final double blackKeyHeight = whiteKeyHeight * 0.6;
+        final double blackKeyWidth = whiteKeyWidth * 0.7;
+        for (int i = 0; i < blackCt; i++) {
+            Button key = blackKey[i] = new Button();
+            key.setStyle("-fx-background-color: #000000;");
+            key.setPrefWidth(blackKeyWidth);
+            key.setPrefHeight(blackKeyHeight);
+            double x = i * whiteKeyWidth + whiteKeyWidth / 2;
+            x += (i + 4) / 5 * whiteKeyWidth + (i + 2) / 5 * whiteKeyWidth;
+            key.setTranslateX(x);
+            nodeList.add(key);
+        }
+    }
 }
